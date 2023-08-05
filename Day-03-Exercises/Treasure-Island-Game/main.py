13206fab7f03f8ff619b972413daa9f49dcb1d93
Huzaifa Haris <119068332+d3xture@users.noreply.github.com>
2023-08-05 16:41:40 +0500

    Create main.py

---
+++ Day-03-Exercises/Treasure-Island-Game/main.py
@@ -0,0 +1,45 @@
+print('''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.") 
+
+#https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
+
+#Write your code below this line 👇
+
+path = input('''You're at a cross road. Where do you want to go? Type "left" or "right"\n''').lower()
+
+if path == "right":
+    print("You fall into a hole. Game Over.")
+else:
+    next = input('''You come to a lake. There is an Island in the middle of the lake. Type "wait" to wait for a boat. Type "swim" to swim across.\n''').lower()
+    if next == "swim":
+        print("You are attacked by trout. Game Over.")
+    else:
+        doors = input('''You arrive at the island unharmed. There is a house with 3 doors. One red, one yellow, and one blue. Which color do you choose?\n''').lower()
+        if doors == "blue":
+            print("You are eaten by beasts. Game Over.")
+        elif doors == "red":
+            print("You are burned by fire. Gamer Over.")
+        else:
+            print("You Win!")
