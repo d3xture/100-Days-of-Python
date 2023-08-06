2ac1cb95012a28326ddecb7d81d593f8f622a993
Huzaifa Haris <119068332+d3xture@users.noreply.github.com>
2023-08-06 09:51:40 +0500

    main.py
    play rock, paper, scissors

--- Day-04-Randomisation-and-Python-Lists/Project-Rock-Paper-Scissors/main.py
+++ Day-04-Randomisation-and-Python-Lists/Project-Rock-Paper-Scissors/main.py
@@ -33,27 +33,31 @@ options = ["rock", "paper", "scissors"]
 game_images = [rock, paper, scissors]
 
 user_choice = input("Enter your choice: (rock, paper, scissors) ").lower()
-print("You Chose")
-print(game_images[options.index(user_choice)])
 
-computer_choice = random.choice(options)
-print("Computer Chose")
-print(game_images[options.index(computer_choice)])
+if user_choice in options:
+    print("You Chose")
+    print(game_images[options.index(user_choice)])
 
-if user_choice == "scissors" and computer_choice == "paper":
-    print("You win!")
-elif user_choice == "paper" and computer_choice == "scissors":
-    print("You lose!")
+    computer_choice = random.choice(options)
+    print("Computer Chose")
+    print(game_images[options.index(computer_choice)])
 
-elif user_choice == "paper" and computer_choice == "rock":
-    print("You win!")
-elif user_choice == "rock" and computer_choice == "paper":
-    print("You lose!")
+    if user_choice == computer_choice:
+        print("It's a draw")
 
-elif user_choice == "rock" and computer_choice == "scissors":
-    print("You win!")
-elif user_choice == "scissors" and computer_choice == "rock":
-    print("You lose!")
+    elif user_choice == "scissors" and computer_choice == "paper":
+        print("You win!")
+    elif user_choice == "paper" and computer_choice == "scissors":
+        print("You lose!")
 
+    elif user_choice == "paper" and computer_choice == "rock":
+        print("You win!")
+    elif user_choice == "rock" and computer_choice == "paper":
+        print("You lose!")
+
+    elif user_choice == "rock" and computer_choice == "scissors":
+        print("You win!")
+    elif user_choice == "scissors" and computer_choice == "rock":
+        print("You lose!")
 else:
-    print("It's a draw!")
+    print("Invalid choice, you lose")
